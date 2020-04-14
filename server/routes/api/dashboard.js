a6874154ff980c6b0cd89bc79a7e66c7e6f225f4
Gabriele Ardizzone <gabri.ardi@gmail.com>
2020-04-14 17:26:40 +0700

    use randomuser.me instead of jsonplaceholder for fake api request

--- server/routes/api/dashboard.js
+++ server/routes/api/dashboard.js
@@ -4,8 +4,8 @@ const passport = require('passport');
 const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  // Fake api request: if user is authenticated and has valid token returns json with photos
-  const url = 'https://jsonplaceholder.typicode.com/photos';
+  // Fake api request: if user is authenticated and has valid token returns json with profiles
+  const url = 'https://randomuser.me/api/?inc=gender,name,nat,picture,location&results=30';
   res.redirect(url);
 });
 
