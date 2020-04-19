807ba49fb70a63e207b78d61cf06a4a0bc2fb2ed
Gabriele Ardizzone <gabri.ardi@gmail.com>
2020-04-19 15:38:15 +0700

    use user id as seed for api request to get same results for same user

--- server/routes/api/dashboard.js
+++ server/routes/api/dashboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 const express = require('express');
 const passport = require('passport');
 
@@ -5,7 +6,9 @@ const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   // Fake api request: if user is authenticated and has valid token returns json with profiles
-  const url = 'https://randomuser.me/api/?inc=gender,name,nat,picture,location&results=30';
+  // Use user id as seed to get same results each time
+  const seed = req.user._id;
+  const url = `https://randomuser.me/api/?inc=gender,name,nat,picture,location&results=30&seed=${seed}`;
   res.redirect(url);
 });
 
